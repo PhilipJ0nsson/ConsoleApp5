c68f0e0609d8657f67af6b3f20fdbceb8e7622af
Philip Jönsson <fillejoensson@hotmail.com>
2023-10-25 14:37:00 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+
+            List<Animal> animals = new List<Animal>();
+            animals.Add(dog);
+            animals.Add(cat);
+
+            foreach (Animal a in animals) 
+            {
+                a.Sound();
+            }
+        }
+    }
+
+
+    class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal");
+        }
+    }
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Cat is saying Mjauuuu mjau");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Dog is saying vooff vooff");
+        }
+    }
+}
